98465af2e0a0b971a8de9e11f0d14528daf3a3f3
Verena Kauth <verena.kauth@gmail.com>
2020-05-06 10:46:45 +0200

    User-Model um Vorname, Nachname ergänzt Datenbank umbenannt auf siebenberger_strolche

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use('/auth', authRouter)
 */
 
 // Start server
-app.listen(5000, () => console.log('Server is up'))
+app.listen(3000, () => console.log('Server is up'))
 /*
 const https = require('https')
 const fs = require('fs')
