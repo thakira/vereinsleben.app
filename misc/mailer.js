9dbae6b6e61613897d2b2698bac42af42879021a
Verena Kauth <verena.kauth@gmail.com>
2020-05-10 16:29:40 +0200

    E-Mail-Verifikation implementiert

---
+++ misc/mailer.js
@@ -0,0 +1,25 @@
+const nodemailer = require("nodemailer")
+
+const MAIL_USER  = 'mailbestaetigung@vereinsleben.app'
+const MAIL_PASS = 'ia;;a48EP%8e'
+
+const transport = nodemailer.createTransport({
+    host: "de10.fcomet.com",
+    port: 465,
+    secure: true, //use TLS
+    auth: {
+        user: MAIL_USER,
+        pass: MAIL_PASS
+    }
+})
+
+module.exports = {
+    sendEmail(from, to, subject, html) {
+        return new Promise((resolve, reject) => {
+            transport.sendMail({from, subject, to, html}, (err, info) => {
+                if(err) reject(err);
+                resolve(info);
+            })
+        })
+    }
+}
--- routes.js
+++ routes.js
@@ -1,6 +1,9 @@
 const User = require('./models/user-model')
 const crypto = require('crypto')
 const LocalStrategy = require('passport-local').Strategy
+const randomString = require('randomstring')
+const mailer = require('./misc/mailer')
+
 
 // Check if user is logged in
 function isLoggedin(req, res, next) {
@@ -14,6 +17,8 @@ function isNotLoggedin(req, res, next) {
     next()
 }
 
+
+
 module.exports = (app, passport) => {
 
     // Username/Password authentication
@@ -23,9 +28,8 @@ module.exports = (app, passport) => {
 
             const hashedPwd = crypto.createHash('sha256').update(password).digest('hex')
             const user = await User.findOne({ email: email, password: hashedPwd })
-
-            if (!user) return done(null, false, 'Login failed!')
-
+            if (!user) return done(null, false, 'Anmeldung fehlgeschlagen!')
+            if(!user.verified) return done(null, false, 'E-Mail wurde noch nicht bestätigt!')
             return done(null, user)
         } catch (exception) {
             done(null, false, exception.message)
@@ -40,6 +44,7 @@ module.exports = (app, passport) => {
         const user = await User.findById(id)
         done(null, user)
     })
+
 
     // Index site
     app.get('/', isNotLoggedin, (req, res) => {
@@ -119,28 +124,24 @@ module.exports = (app, passport) => {
         failureFlash: true
     }))
 
-    /*
-    app.post('/login', isNotLoggedin, async (req, res) => {
-        const email = req.body.email
-        const password = req.body.password
-
+    // User verify
+    app.get('/verify', async(req, res) => {
         try {
-            if (!email || !password) throw { message: 'Empty fields!' }
-
-            const hashedPwd = crypto.createHash('sha256').update(password).digest('hex')
-            const user = await User.findOne({ email: email, password: hashedPwd })
-
-            if (!user) throw { message: 'Login failed!' }
-
-            req.flash('success', 'Login successful.')
-            res.redirect('/dashboard')
-
+            const user = await User.findOne({'secretToken' : req.query['token']})
+            if(!user) {
+                req.flash('error', 'Es gibt keinen Benutzer mit diesem Authentifizierungscode.')
+                res.redirect('/login')
+            }
+            //console.log(req.query['token'])
+            user.verified= true
+            user.secretToken = ''
+            await user.save()
+            req.flash('success', 'Deine E-Mail-Adresse wurde verifiziert. Du kannst Dich jetzt anmelden.')
+            res.redirect('/login')
         } catch (exception) {
             req.flash('error', exception.message)
-            return res.redirect('/login')
         }
     })
-    */
 
     // User logout
     app.get('/logout', isLoggedin, (req, res) => {
@@ -180,16 +181,55 @@ module.exports = (app, passport) => {
             // Hash password
             const hashedPwd = crypto.createHash('sha256').update(password).digest('hex')
 
+            // Generate secret token
+            const secretToken = randomString.generate()
+
+            //flag the acccount as inactive
+            const verified = false
+
+
+            // Store new user
+            await new User({ email: email, password: hashedPwd, firstname: firstname, lastname: lastname, verified: verified, secretToken: secretToken }).save(error => {
+                if (error) throw { message: error.errmsg } // Guard clause
+            })
+            //E-Mail verschicken
+            const html = `Willkommen bei Vereinsleben.app,
+            <br>
+            Um Deine E-Mail-Adresse zu bestätigen, klicke bitte auf folgenden Link:
+            <br>
+            <a href="http://localhost:5000/verify?token=${secretToken}">http://localhost:5000/verify?token=${secretToken}</a>`
+
+            await mailer.sendEmail('mailbestaetigung@vereinsleben.app', email, 'Vereinsmanager.app: Bitte bestätige Deine E-Mail-Adresse', html )
+
+            req.flash('success', 'Du hast es fast geschafft. Wir haben Dir eine E-Mail geschickt. Bitte bestätige Deine Identität, indem Du auf den Link darin klickst.')
+            res.redirect('/login')
+        } catch (exception) {
+            req.flash('error', exception.message)
+            return res.redirect('/login')
+        }
+    })
+
+    // Upload a picture
+    app.post('/profil', isLoggedin, async (req, res) => {
+        try {
+            const image = req.body.image
+            console.log(image)
+
+            if (!image) {
+                console.log('Empty fields')
+                return res.send('Empty fields')
+            }
+            /*req.user.image
             // Store new user
             await new User({ email: email, password: hashedPwd, firstname: firstname, lastname: lastname }).save(error => {
                 if (error) throw { message: error.errmsg } // Guard clause
 
                 req.flash('success', 'Du hast es fast geschafft. Wir haben Dir eine E-Mail geschickt. Bitte bestätige Deine Identität, indem Du auf den Link darin klickst.')
                 res.redirect('/login')
-            })
+            })*/
         } catch (exception) {
             req.flash('error', exception.message)
-            return res.redirect('/login')
+            return res.redirect('/profil')
         }
     })
 
