1bfe05852b84ac2e504372a11b2cf1b002c34299
Marcus Kirschen <marcus.kirschen@stud.hs-emden-leer.de>
2020-05-18 12:17:16 +0200

    Aktuelles implementiert

--- models/news-model.js
+++ models/news-model.js
@@ -5,8 +5,9 @@ const newsSchema = new Schema({
     newsTitle: {type: String, required: true},
     newsText: {type: String, required: true},
     newsImg: { data: Buffer, contentType: String },
-    newsDoc: {type: Mixed},
-    newsReleased: {type: Boolean, default: false}
+    newsDoc: {type: String},
+    newsReleased: {type: Boolean, default: false},
+    newsType: {type: String, default: 'text'} // 'text' || 'image' || 'fullimage'
 }, { timestamps: true })
 
 module.exports = mongoose.model('News', newsSchema)
